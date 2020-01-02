6feb4d8b79c53e76f25065bf2da79d46a7ec8d17
Yuxiang Wu <yuxiang.cs@gmail.com>
2020-01-02 14:27:09 +0000

    Added get_trained_checkpoints

--- src/transformersX/checkpoint_utils.py
+++ src/transformersX/checkpoint_utils.py
@@ -1,14 +1,22 @@
 import os
+import glob
+import logging
 import torch
+
+from transformers import WEIGHTS_NAME
+
+STEP_CKPT = 'checkpoint-{}'
+LAST_CKPT = 'checkpoint-last'
+BEST_CKPT = 'checkpoint-best'
 
 
 def save_checkpoint(args, model, global_step, val_loss=None, tokenizer=None):
     output_dirs = []
     if args.save_all_checkpoints:
-        output_dirs = [os.path.join(args.output_dir, 'checkpoint-{}'.format(global_step))]
+        output_dirs = [os.path.join(args.output_dir, STEP_CKPT.format(global_step))]
 
     if args.save_last_checkpoint:
-        output_dirs.append(os.path.join(args.output_dir, 'checkpoint-last'))
+        output_dirs.append(os.path.join(args.output_dir, LAST_CKPT))
 
     if args.save_best_checkpoint and val_loss is not None:
         # Get the metric for best checkpoint
@@ -18,7 +26,7 @@ def save_checkpoint(args, model, global_step, val_loss=None, tokenizer=None):
         is_better = (lambda x, y: x >= y) if args.maximize_best_checkpoint_metric else (lambda x, y: x <= y)
         if is_better(val_metric, prev_best):
             save_checkpoint.best = val_metric
-            output_dirs.append(os.path.join(args.output_dir, 'checkpoint-best'))
+            output_dirs.append(os.path.join(args.output_dir, BEST_CKPT))
 
     for output_dir in output_dirs:
         if not os.path.exists(output_dir):
@@ -35,3 +43,19 @@ def save_checkpoint(args, model, global_step, val_loss=None, tokenizer=None):
             tokenizer.save_pretrained(output_dir)
 
     return output_dirs
+
+
+def get_trained_checkpoints(args):
+    checkpoints = []
+    if args.save_all_checkpoints:
+        checkpoints = list(
+            os.path.dirname(c) for c in sorted(glob.glob(args.output_dir + '/**/' + WEIGHTS_NAME, recursive=True)))
+        logging.getLogger("transformers.modeling_utils").setLevel(logging.WARN)  # Reduce model loading logs
+    else:
+        if args.save_last_checkpoint:
+            checkpoints.append(os.path.join(args.output_dir, LAST_CKPT))
+        if args.save_best_checkpoint:
+            checkpoints.append(os.path.join(args.output_dir, BEST_CKPT))
+        checkpoints = [c for c in checkpoints if os.path.exists(os.path.join(c, WEIGHTS_NAME))]
+
+    return checkpoints
